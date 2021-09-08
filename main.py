ecbebc1763a87b79c493a6260718d87c1441c1cb
normyp <pm.normington@gmail.com>
2021-09-08 13:55:38 +0100

    Prints out currently connected devices

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
+bus = pydbus.SystemBus()
+
+adapter = bus.get('org.bluez', '/org/bluez/hci0')
+mngr = bus.get('org.bluez', '/')
+
 class ButtonApp(App):
     def build(self):
         layout = BoxLayout(padding=100)
@@ -15,8 +20,18 @@ class ButtonApp(App):
         return layout
 
     def on_press_button(self):
-        print("Hello")
+        list_connected_devices()
+
+def list_connected_devices():
+    mngd_objs = mngr.GetManagedObjects()
+    for path in mngd_objs:
+        con_state = mngd_objs[path].get('org.bluez.Device1', {}).get('Connected', False)
+        if con_state:
+            addr = mngd_objs[path].get('org.bluez.Device1', {}).get('Address')
+            name = mngd_objs[path].get('org.bluez.Device1', {}).get('Name')
+            print(f'Device {name} [{addr}] is connected')
 
 if __name__ == '__main__':
+    list_connected_devices()
     app = ButtonApp()
     app.run()
